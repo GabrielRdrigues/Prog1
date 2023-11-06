79d259fa71bf593707cfd174eb48a721f33014f1
GabrielRdrigues <gabrielrodriguesmarquesvalim@gmail.com>
2023-11-06 20:56:12 -0300

    V0.0

---
+++ SubString.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main(){
+
+    char P[100];
+    char T[100];
+    int i,j;
+
+    scanf("%s",T);
+    scanf("%s",P);
+
+    for(i=0;T[i]!='\0';i++){
+        for(j=0;P[j]!='\0';j++){
+            if(T[i]==P[j]){
+                    
+            }else{
+
+            }
+        }
+    }
+
+
+
+
+
+
+    return 0;
+}
