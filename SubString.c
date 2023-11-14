7696f64d2c90456ddf235940b8e742c4abb535b1
GabrielRdrigues <gabrielrodriguesmarquesvalim@gmail.com>
2023-11-13 22:55:48 -0300

    SubString FINAL

--- SubString.c
+++ SubString.c
@@ -1,28 +1,61 @@
 #include <stdio.h>
 
-int main(){
+int strTam (char V[]){
+    int i,cont=0;
+    for(i=0;V[i]!='\0';i++){
+        cont++;
+    }
+    return cont;
+}
 
-    char P[100];
-    char T[100];
-    int i,j;
+int search(char V[],char W[],int indice,int tamanhoT,int tamanhoP){
+    int i,j=0;;
+    
+    for(i=indice;j<tamanhoP ;i++){
+        if(V[i]==W[j]){
+        }else{
+            return 0;
+        }
+        j++;
+    }
+    if(j==tamanhoP){
+        return 1;
+    }
+    return 0;
+}
+
+int resultado (int result){
+    if(result==1){
+        printf("eh substring");
+        return 1;
+    }else{
+        return 0;
+    }
+}
+
+int main(){
+    int naoeh=0;
+    char P[100],T[100];
+    int i,result,tamanhoT,tamanhoP;
 
     scanf("%s",T);
     scanf("%s",P);
+    tamanhoT=strTam(T);
+    tamanhoP=strTam(P);
 
-    for(i=0;T[i]!='\0';i++){
-        for(j=0;P[j]!='\0';j++){
-            if(T[i]==P[j]){
-                    
-            }else{
-
+    for(i=0;i<=tamanhoT-tamanhoP;i++){
+        if(T[i]==P[0]){
+            result= search(T,P,i,tamanhoT,tamanhoP);
+            if(resultado(result)==1){
+                i=tamanhoT+tamanhoP;
+                naoeh=1;
             }
         }
     }
-
-
-
-
-
+    if(naoeh==0){
+        printf("nao eh substring ok");
+    }
 
     return 0;
 }
+
