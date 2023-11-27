7967562efdaabb56148ea3aa9085aef354ce58e6
GabrielRdrigues <gabrielrodriguesmarquesvalim@gmail.com>
2023-11-26 22:26:03 -0300

    comentario

--- P2Q02V2.c
+++ P2Q02V2.c
@@ -2,8 +2,8 @@
 int main(){
     int n,cont=0;
     char c='@';
-    scanf("%d",&n);
-    while(c!='\n'){
+    scanf("%d",&n); // A leitura deve ser na mesma linha, isto é 2boa sorte
+    while(c!='\n'){ // já que se você pular linha irá ferrar o código, caso queira pular linha colocar scanf("%d\n",&n);
         scanf("%c",&c);
         if(c!='\n'){
             cont++;
